ffb1595eb74f0b382ebdb84c53bfff1d77ac7973
Luciano Ferrari <luciano@bankuish.com>
2023-06-30 16:41:50 -0300

    check for forbidden

--- src/auth/strategy/roles.guard.ts
+++ src/auth/strategy/roles.guard.ts
@@ -5,7 +5,7 @@ import { Role } from 'src/users/enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector) { }
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [context.getHandler(), context.getClass()]);
@@ -13,6 +13,7 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
+    if (!user) { return false; }
 
     return requiredRoles.some((role) => user.roles?.includes(role));
   }
