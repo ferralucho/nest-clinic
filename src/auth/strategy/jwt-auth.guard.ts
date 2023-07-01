fd7abc219f7fbcfb0af9e1810d212b8344cad236
Luciano Ferrari <luciano@bankuish.com>
2023-07-01 09:04:35 -0300

    auth guard activate

--- src/auth/strategy/jwt-auth.guard.ts
+++ src/auth/strategy/jwt-auth.guard.ts
@@ -1,25 +1,32 @@
-import { ExecutionContext, Injectable, Logger, UnauthorizedException } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-  constructor() {
-    super({
-      passReqToCallback: true,
-    });
-  }
+export class JwtAuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
 
-  canActivate(context: ExecutionContext) {
-    // Add your custom authentication logic here
-    // for example, call super.logIn(request) to establish a session.
-    return super.canActivate(context);
-  }
-
-  handleRequest(err, user, info) {
-    // If authentication fails, we throw an UnauthorizedException so that
-    if (err || !user) {
-      throw err || new UnauthorizedException();
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
     }
-    return user;
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.PRIVATE_KEY.replace(/\\\\n/gm, '\\n'),
+      });
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
   }
 }
--- src/ehrs/ehrs.module.ts
+++ src/ehrs/ehrs.module.ts
@@ -3,12 +3,16 @@ import { EHRsService } from './ehrs.service';
 import { EHRsController } from './ehrs.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Conditions, EHR, EHRDiagnosisLabels } from './entities/ehr.entity';
+import { JwtService } from '@nestjs/jwt';
+import { JwtAuthGuard } from 'src/auth/strategy/jwt-auth.guard';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([EHR]),
     TypeOrmModule.forFeature([EHRDiagnosisLabels]),
     TypeOrmModule.forFeature([Conditions]),
+    AuthModule,
   ],
   controllers: [EHRsController],
   providers: [EHRsService],
