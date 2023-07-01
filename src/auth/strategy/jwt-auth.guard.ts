fee9c889a2cdbb3088fd6db69d11d0566b9f4a31
Luciano Ferrari <luciano@bankuish.com>
2023-07-01 10:31:19 -0300

    verify async

--- src/auth/strategy/jwt-auth.guard.ts
+++ src/auth/strategy/jwt-auth.guard.ts
@@ -13,9 +13,7 @@ export class JwtAuthGuard implements CanActivate {
       throw new UnauthorizedException();
     }
     try {
-      const payload = await this.jwtService.verifyAsync(token, {
-        secret: process.env.PUBLIC_KEY.replace(/\\n/gm, '\n'),
-      });
+      const payload = await this.jwtService.verifyAsync(token);
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
       request['user'] = payload;
